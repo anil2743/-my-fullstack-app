93b0a6d77daffd9642f573e5c0dc95b164899581
anil2743 <nakkaanilyadav2743@gmail.com>
2025-11-03 11:28:44 +0530

    Configure frontend-backend communication and CORS

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: '/api',  // Using relative path for Nginx proxy
 });
 
 // Interceptor to add JWT to every request
